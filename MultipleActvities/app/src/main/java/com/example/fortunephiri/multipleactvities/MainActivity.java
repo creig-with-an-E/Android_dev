75bd603ab7c940e377940836060066eb36787447
Creig Fortune <fortunephiri004@gmail.com>
2018-10-27 13:12:52 -0600

    ensuring only a single instance of Activity exists on the activity stack. Instead of creating another activity for the back button you simply use the finish() method

--- MultipleActvities/app/src/main/java/com/example/fortunephiri/multipleactvities/MainActivity.java
+++ MultipleActvities/app/src/main/java/com/example/fortunephiri/multipleactvities/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.fortunephiri.multipleactvities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Test","onCreate");
+
+        findViewById(R.id.btnSecond).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),SecondActivity.class);    //this can be used to start an activity
+                                            //intent takes two params, context and
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("Test","OnStart");
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("Test","OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        //will be triggered when switching to a different activity
+        super.onPause();
+        Log.d("Test","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Test","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Test","onDestroy");
     }
 }
--- MultipleActvities/app/src/main/java/com/example/fortunephiri/multipleactvities/SecondActivity.java
+++ MultipleActvities/app/src/main/java/com/example/fortunephiri/multipleactvities/SecondActivity.java
@@ -1,16 +1,25 @@
 package com.example.fortunephiri.multipleactvities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
+    @Override
+    public void onClick(View v) {
+        finish();   //implementation of the back button, taking the activity off the stack
+        //should be implemented by creating a new Activity as this will add it to the stack
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Log.d("TEST","OnCreate");
+        Log.d("TEST","OnCreate ****SECOND *****");
+        findViewById(R.id.btnBack).setOnClickListener(this);
     }
+
 }
