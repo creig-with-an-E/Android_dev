bdef11208d8f76d0db6479a2ce3439406252b13b
Creig Fortune <fortunephiri004@gmail.com>
2018-11-09 23:44:10 -0500

    adding listView menu.

--- menus/app/src/main/java/com/example/fortunephiri/menus/MainActivity.java
+++ menus/app/src/main/java/com/example/fortunephiri/menus/MainActivity.java
@@ -2,20 +2,83 @@ package com.example.fortunephiri.menus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener , PopupMenu.OnMenuItemClickListener{
 
+    private ActionMode actionMode = null;   //this type of menu is created when using lists or grid
+    private ActionMode.Callback callback = new ActionMode.Callback(){
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater i = mode.getMenuInflater();
+            i.inflate(R.menu.popup_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            //this is the handler for the menu
+            switch (item.getItemId()){
+                case R.id.popup1 :
+                    Toast.makeText(getParent(),"Context is working", Toast.LENGTH_LONG);
+                    return true;
+                default: return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            actionMode = null ;
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.layout).setOnClickListener(this);
+
+        ListView listView =findViewById(R.id.listView);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                //action when adding it to other list
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
+        });
     }
 
     @Override
@@ -37,6 +100,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
 
     @Override
     public void onClick(View v) {
